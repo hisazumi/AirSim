387df20be679081638ba855ac620af5d1fda561f
Kenji Hisazumi <hisazumi@gmail.com>
2023-11-03 20:47:43 +0900

    Update BlocksEditor.Target.cs

--- Unreal/Environments/Blocks/Source/BlocksEditor.Target.cs
+++ Unreal/Environments/Blocks/Source/BlocksEditor.Target.cs
@@ -9,8 +9,13 @@ public class BlocksEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		ExtraModuleNames.AddRange(new string[] { "Blocks" });
-        DefaultBuildSettings = BuildSettingsVersion.V2;
-        //bUseUnityBuild = false;
-        //bUsePCHFiles = false;
+        	DefaultBuildSettings = BuildSettingsVersion.V2;
+        	//bUseUnityBuild = false;
+	        //bUsePCHFiles = false;
+		// fix compile errors
+		bOverrideBuildEnvironment = true;
+		AdditionalCompilerArguments = "-Wno-unused-but-set-variable -Wno-bitwise-instead-of-logical -Wno-unused-but-set-variable -Wno-deprecated-builtins -Wno-single-bit-bitfield-constant-conversion";
+";
+
     }
 }
