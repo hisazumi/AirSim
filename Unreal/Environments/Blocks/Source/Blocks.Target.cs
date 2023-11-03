818d817a2ea37da8cd325c86de0b8b4fa9f725c8
Kenji Hisazumi <hisazumi@gmail.com>
2023-11-03 20:47:16 +0900

    Update Blocks.Target.cs

--- Unreal/Environments/Blocks/Source/Blocks.Target.cs
+++ Unreal/Environments/Blocks/Source/Blocks.Target.cs
@@ -16,7 +16,7 @@ public class BlocksTarget : TargetRules
 
    		// fix compile errors
 		bOverrideBuildEnvironment = true;
-		AdditionalCompilerArguments = "-Wno-unused-but-set-variable -Wno-bitwise-instead-of-logical -Wno-unused-but-set-variable";
-
+		AdditionalCompilerArguments = "-Wno-unused-but-set-variable -Wno-bitwise-instead-of-logical -Wno-unused-but-set-variable -Wno-deprecated-builtins -Wno-single-bit-bitfield-constant-conversion";
+";
 	}
 }
